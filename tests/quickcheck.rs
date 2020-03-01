7a9c3148cc4159a14724e50fb85c5a9c3151e5e1
Joakim Frostegård <joakim.frostegard@gmail.com>
2020-03-01 13:47:25 +0100

    quickcheck: test fma, abs and sqrt

--- tests/quickcheck.rs
+++ tests/quickcheck.rs
@@ -1,3 +1,6 @@
+/// QuickCheck tests
+/// 
+/// TODO: test shifts
 #[cfg(test)]
 mod tests {
     use core::ops::*;
@@ -17,7 +20,7 @@ mod tests {
 
     impl SpecialEq for f64 {
         fn equivalent(a: Self, b: Self) -> bool {
-            if a.is_nan() && b.is_nan() {
+            if (a.is_nan() && b.is_nan()) || (a.is_infinite() && b.is_infinite()) {
                 a.to_bits() == b.to_bits()
             } else {
                 (a - b).abs() < 0.00000001
@@ -26,7 +29,7 @@ mod tests {
     }
     impl SpecialEq for f32 {
         fn equivalent(a: Self, b: Self) -> bool {
-            if a.is_nan() && b.is_nan() {
+            if (a.is_nan() && b.is_nan()) || (a.is_infinite() && b.is_infinite()) {
                 a.to_bits() == b.to_bits()
             } else {
                 (a - b).abs() < 0.00000001
@@ -43,6 +46,8 @@ mod tests {
             a == b
         }
     }
+
+    // Double argument operations
 
     macro_rules! gen_quickcheck_2_simd {
         ($fn_name:ident, $operation_scalar:expr, $operation_simd:expr, $ty:ty, $width:ident, $set_fn:ident) => {
@@ -182,4 +187,160 @@ mod tests {
     gen_quickcheck_2_simd!(andnot_i64, (|a: i64, b: i64| !a & b), S::andnot_epi64, i64, VI64_WIDTH, set1_epi64);
     gen_quickcheck_2_simd!(andnot_f32, (|a: f32, b: f32| f32::from_bits((!a.to_bits()) & b.to_bits()) ), S::andnot_ps, f32, VF32_WIDTH, set1_ps);
     gen_quickcheck_2_simd!(andnot_f64, (|a: f64, b: f64| f64::from_bits((!a.to_bits()) & b.to_bits()) ), S::andnot_pd, f64, VF64_WIDTH, set1_pd);
+
+
+    // Triple argument operations (e.g., fused multiply-add)
+
+    macro_rules! gen_quickcheck_3_simd {
+        ($fn_name:ident, $operation_scalar:expr, $operation_simd:expr, $ty:ty, $width:ident, $set_fn:ident) => {
+            simd_runtime_generate!(
+                fn $fn_name(a: $ty, b: $ty, c: $ty) -> bool {
+                    let a_simd = S::$set_fn(a);
+                    let b_simd = S::$set_fn(b);
+                    let c_simd = S::$set_fn(b);
+
+                    let result_simd = $operation_simd(a_simd, b_simd, c_simd);
+                    let result_scalar = $operation_scalar(a, b, c);
+
+                    for i in 0..S::$width {
+                        if !SpecialEq::equivalent(result_simd[i], result_scalar) {
+                            println!(
+                                "values didn't match. reference: {:?} simdeez: {:?}",
+                                result_scalar,
+                                result_simd
+                            );
+
+                            return false;
+                        }
+                    }
+
+                    true
+                }
+            );
+
+            paste::item! {
+                #[test]
+                fn [<test_ $fn_name>](){
+                    fn prop(data: ($ty, $ty, $ty)) -> TestResult {
+                        let a = data.0;
+                        let b = data.1;
+                        let c = data.1;
+
+                        let mut ok = true;
+
+                        unsafe {
+                            println!("testing scalar:");
+                            ok &= [<$fn_name _scalar>](a, b, c);
+
+                            if is_x86_feature_detected!("sse2"){
+                                println!("\n testing sse2:");
+                                ok &= [<$fn_name _sse2>](a, b, c);
+                            }
+                            if is_x86_feature_detected!("sse4.1"){
+                                println!("\n testing sse41:");
+                                ok &= [<$fn_name _sse41>](a, b, c);
+                            }
+                            if is_x86_feature_detected!("avx"){
+                                println!("\n testing avx:");
+                                ok &= [<$fn_name _avx>](a, b, c);
+                            }
+                            if is_x86_feature_detected!("avx2"){
+                                println!("\n testing avx2:");
+                                ok &= [<$fn_name _avx2>](a, b, c);
+                            }
+                        }
+
+                        TestResult::from_bool(ok)
+                    }
+
+                    quickcheck(prop as fn(($ty, $ty, $ty)) -> TestResult);
+                }
+            }
+        };
+    }
+
+    gen_quickcheck_3_simd!(fmadd_f32, (|a, b, c| a * b + c), S::fmadd_ps, f32, VF32_WIDTH, set1_ps);
+    gen_quickcheck_3_simd!(fmadd_f64, (|a, b, c| a * b + c), S::fmadd_pd, f64, VF64_WIDTH, set1_pd);
+
+    gen_quickcheck_3_simd!(fnmadd_f32, (|a, b, c| -1.0f32 * a * b + c), S::fnmadd_ps, f32, VF32_WIDTH, set1_ps);
+    gen_quickcheck_3_simd!(fnmadd_f64, (|a, b, c| -1.0f64 * a * b + c), S::fnmadd_pd, f64, VF64_WIDTH, set1_pd);
+
+    gen_quickcheck_3_simd!(fmsub_f32, (|a, b, c| a * b - c), S::fmsub_ps, f32, VF32_WIDTH, set1_ps);
+    gen_quickcheck_3_simd!(fmsub_f64, (|a, b, c| a * b - c), S::fmsub_pd, f64, VF64_WIDTH, set1_pd);
+
+    gen_quickcheck_3_simd!(fnmsub_f32, (|a, b, c| -1.0f32 * a * b - c), S::fnmsub_ps, f32, VF32_WIDTH, set1_ps);
+    gen_quickcheck_3_simd!(fnmsub_f64, (|a, b, c| -1.0f64 * a * b - c), S::fnmsub_pd, f64, VF64_WIDTH, set1_pd);
+
+
+    // Single argument operations
+
+    macro_rules! gen_quickcheck_1_simd {
+        ($fn_name:ident, $operation_scalar:expr, $operation_simd:expr, $ty:ty, $width:ident, $set_fn:ident, $discard_value_lambda:expr) => {
+            simd_runtime_generate!(
+                fn $fn_name(a: $ty) -> bool {
+                    let result_simd = $operation_simd(S::$set_fn(a));
+                    let result_scalar = $operation_scalar(a);
+
+                    for i in 0..S::$width {
+                        if !SpecialEq::equivalent(result_simd[i], result_scalar) {
+                            println!(
+                                "Values didn't match. Reference: {:?} Simdeez: {:?}",
+                                result_scalar,
+                                result_simd
+                            );
+
+                            return false;
+                        }
+                    }
+
+                    true
+                }
+            );
+
+            paste::item! {
+                #[test]
+                fn [<test_ $fn_name>](){
+                    fn prop(a: $ty) -> TestResult {
+                        if ($discard_value_lambda)(a){
+                            return TestResult::discard();
+                        }
+
+                        let mut ok = true;
+
+                        unsafe {
+                            println!("testing scalar:");
+                            ok &= [<$fn_name _scalar>](a);
+
+                            if is_x86_feature_detected!("sse2"){
+                                println!("\n testing sse2:");
+                                ok &= [<$fn_name _sse2>](a);
+                            }
+                            if is_x86_feature_detected!("sse4.1"){
+                                println!("\n testing sse41:");
+                                ok &= [<$fn_name _sse41>](a);
+                            }
+                            if is_x86_feature_detected!("avx"){
+                                println!("\n testing avx:");
+                                ok &= [<$fn_name _avx>](a);
+                            }
+                            if is_x86_feature_detected!("avx2"){
+                                println!("\n testing avx2:");
+                                ok &= [<$fn_name _avx2>](a);
+                            }
+                        }
+
+                        TestResult::from_bool(ok)
+                    }
+
+                    quickcheck(prop as fn($ty) -> TestResult);
+                }
+            }
+        };
+    }
+
+    gen_quickcheck_1_simd!(abs_f32, f32::abs, S::abs_ps, f32, VF32_WIDTH, set1_ps, |_| false);
+    gen_quickcheck_1_simd!(abs_f64, f64::abs, S::abs_pd, f64, VF64_WIDTH, set1_pd, |_| false);
+
+    gen_quickcheck_1_simd!(sqrt_f32, f32::sqrt, S::sqrt_ps, f32, VF32_WIDTH, set1_ps, |a: f32| a.is_sign_negative());
+    gen_quickcheck_1_simd!(sqrt_f64, f64::sqrt, S::sqrt_pd, f64, VF64_WIDTH, set1_pd, |a: f64| a.is_sign_negative());
 }
