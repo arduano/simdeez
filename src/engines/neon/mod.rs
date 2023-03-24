269bd5aadd8ef1dc331e534b528e753e05301abc
Arduano <leonid.shchurov@gmail.com>
2023-03-24 23:10:58 +1100

    Cleaned up the test suite

--- src/engines/neon/mod.rs
+++ src/engines/neon/mod.rs
@@ -13,26 +13,26 @@ use crate::ops::*;
 mod simd;
 pub use self::simd::*;
 
-define_simd_type!(i8, 16, int8x16_t, _N);
-impl_simd_int_overloads!(I8x16_N);
-impl_i8_simd_type!(Neon, I8x16_N, I16x8_N);
+define_simd_type!(i8, 16, int8x16_t, Neon);
+impl_simd_int_overloads!(I8x16Neon);
+impl_i8_simd_type!(Neon, I8x16Neon, I16x8Neon);
 
-define_simd_type!(i16, 8, int16x8_t, _N);
-impl_simd_int_overloads!(I16x8_N);
-impl_i16_simd_type!(Neon, I16x8_N, I32x4_N);
+define_simd_type!(i16, 8, int16x8_t, Neon);
+impl_simd_int_overloads!(I16x8Neon);
+impl_i16_simd_type!(Neon, I16x8Neon, I32x4Neon);
 
-define_simd_type!(i32, 4, int32x4_t, _N);
-impl_simd_int_overloads!(I32x4_N);
-impl_i32_simd_type!(Neon, I32x4_N, F32x4_N, I64x2_N);
+define_simd_type!(i32, 4, int32x4_t, Neon);
+impl_simd_int_overloads!(I32x4Neon);
+impl_i32_simd_type!(Neon, I32x4Neon, F32x4Neon, I64x2Neon);
 
-define_simd_type!(i64, 2, int64x2_t, _N);
-impl_simd_int_overloads!(I64x2_N);
-impl_i64_simd_type!(Neon, I64x2_N, F64x2_N);
+define_simd_type!(i64, 2, int64x2_t, Neon);
+impl_simd_int_overloads!(I64x2Neon);
+impl_i64_simd_type!(Neon, I64x2Neon, F64x2Neon);
 
-define_simd_type!(f32, 4, float32x4_t, _N);
-impl_simd_float_overloads!(F32x4_N);
-impl_f32_simd_type!(Neon, F32x4_N, I32x4_N);
+define_simd_type!(f32, 4, float32x4_t, Neon);
+impl_simd_float_overloads!(F32x4Neon);
+impl_f32_simd_type!(Neon, F32x4Neon, I32x4Neon);
 
-define_simd_type!(f64, 2, float64x2_t, _N);
-impl_simd_float_overloads!(F64x2_N);
-impl_f64_simd_type!(Neon, F64x2_N, I64x2_N);
+define_simd_type!(f64, 2, float64x2_t, Neon);
+impl_simd_float_overloads!(F64x2Neon);
+impl_f64_simd_type!(Neon, F64x2Neon, I64x2Neon);
--- src/engines/neon/simd.rs
+++ src/engines/neon/simd.rs
@@ -3,12 +3,12 @@ use crate::Simd;
 
 pub struct Neon;
 impl Simd for Neon {
-    type Vi8 = I8x16_N;
-    type Vi16 = I16x8_N;
-    type Vi32 = I32x4_N;
-    type Vf32 = F32x4_N;
-    type Vf64 = F64x2_N;
-    type Vi64 = I64x2_N;
+    type Vi8 = I8x16Neon;
+    type Vi16 = I16x8Neon;
+    type Vi32 = I32x4Neon;
+    type Vf32 = F32x4Neon;
+    type Vf64 = F64x2Neon;
+    type Vi64 = I64x2Neon;
 
     #[inline]
     fn invoke<R>(f: impl FnOnce() -> R) -> R {
