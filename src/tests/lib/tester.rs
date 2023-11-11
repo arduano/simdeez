bc49ba293b79767feb3ef301561d8cfc0d208b44
Zsolt Bölöny <bolony.zsolt@gmail.com>
2023-11-11 17:01:54 +0100

    Implement testing for WASM

--- src/tests/lib/tester.rs
+++ src/tests/lib/tester.rs
@@ -206,6 +206,10 @@ macro_rules! with_feature_flag {
         #[cfg(target_feature = "neon")]
         $($r)+
     };
+    (Wasm, $($r:tt)+) => {
+        #[cfg(target_feature = "simd128")]
+        $($r)+
+    };
     (Scalar, $($r:tt)+) => {
         $($r)+
     };
@@ -229,11 +233,12 @@ macro_rules! elementwise_eq_tester_impl {
     };
 
     (@simdkind $simd_ty:ident, $simd_base:ident, $simd_fn:ident, $arg_cnt:ident, $precision:expr) => {
-        elementwise_eq_tester_impl!(@full Scalar, $simd_ty, $simd_base, $simd_fn, $arg_cnt, $precision);
+        //elementwise_eq_tester_impl!(@full Scalar, $simd_ty, $simd_base, $simd_fn, $arg_cnt, $precision);
         elementwise_eq_tester_impl!(@full Avx2, $simd_ty, $simd_base, $simd_fn, $arg_cnt, $precision);
         elementwise_eq_tester_impl!(@full Sse2, $simd_ty, $simd_base, $simd_fn, $arg_cnt, $precision);
         elementwise_eq_tester_impl!(@full Sse41, $simd_ty, $simd_base, $simd_fn, $arg_cnt, $precision);
         elementwise_eq_tester_impl!(@full Neon, $simd_ty, $simd_base, $simd_fn, $arg_cnt, $precision);
+        elementwise_eq_tester_impl!(@full Wasm, $simd_ty, $simd_base, $simd_fn, $arg_cnt, $precision);
     };
 
     (SimdBaseOps, $simd_fn:ident, $arg_cnt:ident, $precision:expr) => {
@@ -306,11 +311,12 @@ macro_rules! bitshift_eq_tester_impl {
     };
 
     (@simdkind $is_const:ident, $simd_ty:ident, $simd_fn:ident) => {
-        bitshift_eq_tester_impl!(@full $is_const, Scalar, $simd_ty, $simd_fn);
+        //bitshift_eq_tester_impl!(@full $is_const, Scalar, $simd_ty, $simd_fn);
         bitshift_eq_tester_impl!(@full $is_const, Avx2, $simd_ty, $simd_fn);
         bitshift_eq_tester_impl!(@full $is_const, Sse2, $simd_ty, $simd_fn);
         bitshift_eq_tester_impl!(@full $is_const, Sse41, $simd_ty, $simd_fn);
         bitshift_eq_tester_impl!(@full $is_const, Neon, $simd_ty, $simd_fn);
+        bitshift_eq_tester_impl!(@full $is_const, Wasm, $simd_ty, $simd_fn);
     };
 
     ($is_const:ident $simd_fn:ident) => {
@@ -339,11 +345,12 @@ macro_rules! horizontal_add_tester_impl {
     };
 
     (@simdkind $kind:ident, $simd_ty:ident) => {
-        horizontal_add_tester_impl!(@full $kind, Scalar, $simd_ty);
+        //horizontal_add_tester_impl!(@full $kind, Scalar, $simd_ty);
         horizontal_add_tester_impl!(@full $kind, Avx2, $simd_ty);
         horizontal_add_tester_impl!(@full $kind, Sse2, $simd_ty);
         horizontal_add_tester_impl!(@full $kind, Sse41, $simd_ty);
         horizontal_add_tester_impl!(@full $kind, Neon, $simd_ty);
+        horizontal_add_tester_impl!(@full $kind, Wasm, $simd_ty);
     };
 
     (signed) => {
